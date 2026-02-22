78f54ad534a0b939fb1225fbb6a8dc85e8d5973c
fiyinfholuwa <fiyinfholuwa@gmail.com>
2026-02-22 19:20:17 +0100

    fixies

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -95,7 +95,7 @@ class ContactController extends Controller
     // Set the headers for HTML email
     $headers = "MIME-Version: 1.0\r\n";
     $headers .= "Content-type: text/html; charset=UTF-8\r\n";
-    $headers .= "From: Ministry <noreply@fclmng.org>\r\n";
+    $headers .= "From: FCLM Ministry <noreply@fclmng.org>\r\n";
     $headers .= "Reply-To: noreply@fclmng.org\r\n";
 
     // Send the email
